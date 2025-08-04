1c97ba20a9c809553175795a04f068c6e90e08b6
Mohamuzair1 <153000612+Mohamuzair1@users.noreply.github.com>
2025-08-05 00:24:11 +0530

    Delete frontend/backend directory

--- frontend/backend/start-backend-simple.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0..\\backend"
-echo Starting Connexa Backend Server...
-npm run dev
-pause
--- frontend/backend/start-frontend-simple.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0..\\frontend"
-echo Starting Connexa Frontend...
-npm run dev
-pause
